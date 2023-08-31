dde5078731fb9a9bc774eb2d854cbc30bbf3ec1e
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2023-08-31 19:07:46 -0300

    new: products images

---
+++ assets/Products/products.ts
@@ -0,0 +1,24 @@
+import CocaCola from './CocaCola.png';
+import Pepsi from './Pepsi.jpg';
+import PaoFrances from './PaoFrances.png';
+import Coxinha from './Coxinha.png';
+import PaoDeQueijo from './PaoDeQueijo.png';
+import Brigadeiro from './Brigadeiro.png';
+import Beijinho from './Beijinho.png';
+import BoloDeChocolate from './BoloDeChocolate.png';
+import BoloDeLimao from './BoloDeLimao.png';
+import SanduicheDeFrango from './SanduicheDeFrango.png';
+
+
+export {
+    CocaCola,
+    Pepsi,
+    PaoFrances,
+    Coxinha,
+    PaoDeQueijo,
+    Brigadeiro,
+    Beijinho,
+    BoloDeChocolate,
+    BoloDeLimao,
+    SanduicheDeFrango,
+};
