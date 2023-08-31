a45c63471721c660dde1d2d01befd89399324a54
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2023-08-31 16:31:42 -0300

    fix: add ScrollView to scroll down main page

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
 import Main from './pages/Main/Main';
 import Header from './components/Header/Header';
 
 export default function App() {
   return <>
-    <SafeAreaView style={{
-        backgroundColor: '#F1F1F1', 
-        minHeight: '100%'
-      }}>
-      <StatusBar />
-      <Header />
-      <Main />
-    </SafeAreaView>
+    <ScrollView>
+      <SafeAreaView style={{
+          backgroundColor: '#F1F1F1', 
+          minHeight: '100%'
+        }}>
+        <StatusBar />
+        <Header />
+        <Main />
+      </SafeAreaView>
+    </ScrollView>
   </>;
 }
